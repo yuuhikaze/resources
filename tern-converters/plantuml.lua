347469af8ba10420a44e0c4a5c94a322f08051d2
yuuhikaze <yuuhikaze@protonmail.com>
2026-02-17 14:48:04 -0500

    fix: handle plantuml exporting logic correctly

--- tern-converters/plantuml.lua
+++ tern-converters/plantuml.lua
@@ -1,14 +1,23 @@
 function concat_with_space(...)
-  return table.concat({...}, " ")
+    return table.concat({ ... }, " ")
+end
+
+function remove_filename_from_path(path)
+    path = path:gsub("\\", "/")
+    return string.match(path, "^(.*)/[^/]*$") or path
 end
 
 --- @param input string: The source file/directory
 --- @param output string: The destination directory
 --- @param options table: A table of strings like {"-tsvg", "-v"}
 function convert(input, output, options)
-    -- structurizr export ... -workspace workspace.dsl -output diagrams
-    -- -format static
-    local command = concat_with_space("plantuml", options[1], "-o", output, input)
+    -- $(realpath %s) is needed bc plantuml is a smartass
+    local command = string.format(
+        'plantuml %s -o "$(realpath %s)" %s',
+        options[1] or "-tsvg",
+        remove_filename_from_path(output),
+        input
+    )
     return os.execute(command)
 end
 
